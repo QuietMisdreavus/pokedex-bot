7cf39db0a8d41b9b8813456f0615c69eac2e8f70
Bryan 'icesoldier' Mitchell <bryan@icesoldier.me>
2016-03-15 20:52:23 -0500

    save a user's last search (and track it across nick changes)

--- src/main.rs
+++ src/main.rs
@@ -83,19 +83,34 @@ fn main() {
 
     println!("Ready!");
 
+    let mut last_search = HashMap::new();
+
     for message in srv.iter() {
         let message = message.unwrap();
         match message.command {
             Command::PRIVMSG(ref target, ref msg) => if msg.len() >= 4 {
-                let (cmd, body) = msg.split_at(4);
-                if cmd == "!dex" {
-                    println!("{}: {:?}: {}", target, message.source_nickname(), msg);
-                    if body.trim().len() == 0 {
-                        println!("----Empty command!");
-                    } else if let Some(id) = get_id(body.trim(), &db) {
-                        srv.send_privmsg(target, &print_poke(&id, &db)).unwrap();
-                    } else {
-                        srv.send_privmsg(target, "Sorry, that's not a pokemon I know of.").unwrap();
+                if let Some(nick) = message.source_nickname() {
+                    let (cmd, body) = msg.split_at(4);
+                    if cmd == "!dex" {
+                        println!("{}: {}: {}", target, nick, msg);
+                        if body.trim().len() == 0 {
+                            if let Some(id) = last_search.get(nick) {
+                                srv.send_privmsg(target, &print_poke(id, &db)).unwrap();
+                            }
+                        } else if let Some(id) = get_id(body.trim(), &db) {
+                            srv.send_privmsg(target, &print_poke(&id, &db)).unwrap();
+                            last_search.insert(nick.to_owned(), id);
+                        } else {
+                            srv.send_privmsg(target, "Sorry, that's not a pokemon I know of.").unwrap();
+                        }
+                    }
+                }
+            },
+            Command::NICK(ref new_nick) => {
+                if let Some(nick) = message.source_nickname() {
+                    println!("Nick change: {} -> {}", nick, new_nick);
+                    if let Some(id) = last_search.remove(nick) {
+                        last_search.insert(new_nick.to_owned(), id);
                     }
                 }
             },
