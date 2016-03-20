6faf4a6fd8e8d8b0c5ffd7c80bdcc3c1ce5762d8
Bryan 'icesoldier' Mitchell <bryan@icesoldier.me>
2016-03-19 22:13:18 -0500

    require second argument of starts_with to be shorter than the first

--- src/misc.rs
+++ src/misc.rs
@@ -1,4 +1,7 @@
 pub fn starts_with(s1: &str, s2: &str) -> bool {
+    if s1.len() < s2.len() {
+        return false;
+    }
     for (c1, c2) in s1.chars().zip(s2.chars()) {
         if c1 != c2 {
             return false;
