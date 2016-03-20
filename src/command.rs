db84085cb6cef6ebf7de411752b69ccd806bfa46
Bryan 'icesoldier' Mitchell <bryan@icesoldier.me>
2016-03-20 17:56:08 -0500

    better command handling rather than my flimsy starts_with

--- src/command.rs
+++ src/command.rs
@@ -1,15 +1,18 @@
-use misc;
-
 pub enum Command<'a> {
     Pokemon(&'a str)
 }
 
 impl<'a> Command<'a> {
     pub fn from_str(cmd: &'a str) -> Command {
-        if misc::starts_with(cmd, "pokemon") {
-            Command::Pokemon(&cmd[7..])
-        } else if misc::starts_with(cmd, "p") {
-            Command::Pokemon(&cmd[1..])
+        let mut iter = cmd.split_whitespace();
+        if let Some(word) = iter.next() {
+            if word == "pokemon" {
+                Command::Pokemon(&cmd[7..])
+            } else if word == "p" {
+                Command::Pokemon(&cmd[1..])
+            } else {
+                Command::Pokemon(cmd)
+            }
         } else {
             Command::Pokemon(cmd)
         }
